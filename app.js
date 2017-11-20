42245a391a4d877ce4d7a0ed44256b7be89632fc
Sam Sheehan <ssheehan1210@yahoo.com>
2017-11-20 11:47:01 -0600

    finished initial readme lab assignments

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = angular.module('MyApp', []);
 
 
 // initialize a controller
-app.controller('MyController', [function(){ // constructor function called MyController
+app.controller('MyController', ['$http', function($http){ // constructor function called MyController
 	this.hello = 'Hey Buddy';
 	this.isActive = true;
 	this.items = ['apple', 'banana', 'pear'];
@@ -12,10 +12,27 @@ app.controller('MyController', [function(){ // constructor function called MyCon
 	this.showDiv = false;
 	this.imgSrc = 'https://i.ytimg.com/vi/mRf3-JkwqfU/hqdefault.jpg';
 	this.someProperty = 'Yellow';
+	this.toDoText = '';
+	this.toDoList = [];
+	this.editedText = '';
 	this.handleSubmit = function(){
-		console.log('form has been submitted');
+		console.log('form is submitting');
+		this.toDoList.push(this.toDoText);
+		console.log('form has submitted');
 	},
 	this.handleClick = function(){
-		console.log('div is clicked')
+		console.log('div is clicked');
+	},
+	this.deleteTask = function(index){
+		console.log('deleting selected task');
+		this.toDoList.splice(index, 1);
+	},
+	this.revealEditor = function(){
+		console.log('revealing hidden edit div');
+		this.showDiv = true;
+	},
+	this.handleEdit = function(index, string){
+		console.log('edit is being made');
+		this.toDoList[index] = string;
 	}
-}])
+}]);
