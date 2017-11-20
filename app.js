17a6e7800a1f069a7d905fcf3edc6190d116f6a1
Sam Sheehan <ssheehan1210@yahoo.com>
2017-11-20 12:57:46 -0600

    Added functionality for Json/Ajax to create unordered lists

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const app = angular.module('MyApp', []);
 
-
-
 // initialize a controller
 app.controller('MyController', ['$http', function($http){ // constructor function called MyController
+	this.resultsArray = [];
 	this.hello = 'Hey Buddy';
 	this.isActive = true;
 	this.items = ['apple', 'banana', 'pear'];
@@ -34,5 +33,29 @@ app.controller('MyController', ['$http', function($http){ // constructor functio
 	this.handleEdit = function(index, string){
 		console.log('edit is being made');
 		this.toDoList[index] = string;
+	},
+	this.getCrimes = function(){
+		console.log(this); // Shows all the functions and objects within the window
+		const ctrl = this; // This controller variable will now equal all of the possible functions that can be done
+		$http({
+			method: 'GET',
+			url: 'https://data.cityofchicago.org/resource/crimes.json'
+		}).then(function(response){
+			// successful callback
+			console.log(this); // Shows all the functions and objects within the window
+			console.log(response);
+			for (let i = 0; i < response.data.length; i++) {
+				const caseNumber = response.data[i].case_number;
+				const currentDescription = response.data[i].description;
+				const combinedInfo = 'Case Number: ' + caseNumber + ', Description: ' + currentDescription;
+				const resResult = {crimeText: combinedInfo};
+				ctrl.resultsArray.push(resResult);
+			}
+			console.log(ctrl.resultsArray);
+			console.log("finished looping through data");
+		}, function(err){
+			// failure callback
+			console.log(err);
+		});
 	}
 }]);
